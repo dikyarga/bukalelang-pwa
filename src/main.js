60e80557977c7dcb9e371e09867c3ccded3b770f
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-10-18 02:48:52 +0700

    create login and signup feature

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
+import * as firebase from 'firebase'
 import Vuetify from 'vuetify'
 
 import App from './App'
 import router from './router'
+import { store } from './store'
+
 
 Vue.config.productionTip = false
 require('../node_modules/vuetify/dist/vuetify.min.css')
@@ -15,6 +19,29 @@ Vue.use(Vuetify)
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  created() {
+    const config = {
+      apiKey: "AIzaSyBl1x_7NdJFoQp2yS-WSBnRPgih8etkQJY",
+      authDomain: "bukalelang-3d2a3.firebaseapp.com",
+      databaseURL: "https://bukalelang-3d2a3.firebaseio.com",
+      projectId: "bukalelang-3d2a3",
+      storageBucket: "bukalelang-3d2a3.appspot.com",
+      messagingSenderId: "373989807666"
+    };
+    firebase.initializeApp(config)
+
+    firebase.auth().onAuthStateChanged(user => {
+      console.log('first ?', user)
+      if (user) {
+        // this.$store.commit('setUser', user)
+        this.$store.dispatch('autoSignIn', user)
+        // this.$store.dispatch('loadAuction')
+      } else {
+        this.$store.dispatch('errorLogin')
+      }
+    })
+  }
 })
