408642016c59cefe620a0eaac5687bdd3d261be7
Diky Arga <dikyarga.id@gmail.com>
2017-10-17 23:50:13 +0700

    add foundation components

---
+++ src/components/Layout/MainLayout.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <v-container>
+     <v-layout row>
+       <v-flex xs12 sm10 offset-sm1>
+        <router-view></router-view>
+      </v-flex>
+    </v-layout>
+ </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
